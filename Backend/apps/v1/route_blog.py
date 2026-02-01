83a00d1df3ba407ac9db0644c2fb7bb5f9fd662f
Priyanka <priyankakedar96@gmail.com>
2026-02-01 17:21:59 +0530

    adding frontend code

---
+++ Backend/apps/v1/route_blog.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi import Depends
+from typing import Optional
+from sqlalchemy.orm import Session
+from db.repository.blog import list_all_blogs
+from db.repository.blog import retrieve_blog 
+from db.session import get_db
+from fastapi.templating import Jinja2Templates
+from fastapi import Form, responses, status
+from schemas.blog import CreateBlog
+from apis.v1.route_login import get_current_user
+from db.repository.blog import create_new_blog
+from fastapi.security.utils import get_authorization_scheme_param
+from db.repository.blog import delete_blog
+
+templates = Jinja2Templates(directory="templates")
+router = APIRouter()
+
+@router.get("/")
+def home(request: Request, alert: Optional[str] = None, db: Session = Depends(get_db)):
+    blogs = list_all_blogs(db=db)
+    return templates.TemplateResponse("blog/home.html", {"request": request, "blogs": blogs, "alert":alert})
+
+@router.get("/app/blog/{id}")
+def blog_detail(request: Request, id: int, db: Session = Depends(get_db)):
+    blog = retrieve_blog(id=id, db=db)
+    return templates.TemplateResponse(
+        "blog/detail.html", {"request": request, "blog": blog}
+    )
+
+@router.get("/app/create-new-blog")
+def create_blog(request: Request):
+    return templates.TemplateResponse("blog/create_blog.html",{"request":request})
+
+@router.post("/app/create-new-blog")
+def create_blog(
+    request: Request,
+    title: str = Form(...),
+    content: str = Form(...),
+    db: Session = Depends(get_db),
+):
+    token = request.cookies.get("access_token")
+    _, token = get_authorization_scheme_param(token)
+    try:
+        author = get_current_user(token=token, db=db)
+        blog = CreateBlog(title=title, content=content)
+        blog = create_new_blog(blog=blog, db=db, author_id=author.id)
+        return responses.RedirectResponse(
+            "/?alert=Blog Submitted for Review", status_code=status.HTTP_302_FOUND
+        )
+    except Exception as e:
+        errors = ["Please log in to create blog"]
+        print("Exception raised", e)
+        return templates.TemplateResponse(
+            "blog/create_blog.html",
+            {"request": request, "errors": errors, "title": title, "content": content},
+        )
+
+@router.get("/delete/{id}")
+def delete_a_blog(request: Request,id: int, db:Session = Depends(get_db)):
+    token = request.cookies.get("access_token")
+    _, token = get_authorization_scheme_param(token)
+    try:
+        author = get_current_user(token=token, db=db)
+        msg = delete_blog(id=id,author_id=author.id,db=db)
+        alert = msg.get("error") or msg.get("msg")
+        return responses.RedirectResponse(
+            f"/?alert={alert}", status_code=status.HTTP_302_FOUND
+        )
+    except Exception as e:
+        print(f"Exception raised while deleting {e}")
+        blog = retrieve_blog(id=id,db=db)
+        return templates.TemplateResponse("blog/detail.html",{"request":request,"alert":"Please Login Again",
+                                                              "blog":blog})
--- Backend/main.py
+++ Backend/main.py
@@ -3,26 +3,36 @@ from core.config import settings
 from db.session import engine 
 from db.base import Base 
 from apis.base import api_router
+from apps.base import app_router
+from fastapi.staticfiles import StaticFiles
 
 """def create_tables():         
 	Base.metadata.create_all(bind=engine)"""
 
-def include_router(app):   
-	app.include_router(api_router)
+def include_router(app):
+    app.include_router(api_router)
+    app.include_router(app_router)
+
+
+def configure_staticfiles(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
         
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
     #create_tables()
     include_router(app)
+    configure_staticfiles(app)
     return app
 
 
 app = start_application()
 
-
+"""
 @app.get("/")
 def home():
     return {"msg":"Welcome to Blog App using FastAPI🚀"}
+"""
+
 
 
